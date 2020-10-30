bc877f8c6e3334223b8e622db16f6b27a41fd620
Debadri3 <62327777+Debadri3@users.noreply.github.com>
2020-10-31 00:46:47 +0530

    Added NSEI data

---
+++ datasets/nsei_closing_prices/info.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import r2_score
+from benchmarks.helpers.confidence import confidence_from_acc_v1
+
+file = 'Boston.csv'
+speed = 'fast'
+active = True
+accuracy_function = r2_score
+confidence_function = confidence_from_acc_v1
+target = 'Close'
